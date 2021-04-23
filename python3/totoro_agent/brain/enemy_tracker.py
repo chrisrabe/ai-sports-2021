f5686aa5ff6b7b6b1b9e097cbcc605796e815ab1
Anti Matter <spiralknightsowner@gmail.com>
2021-04-23 23:59:31 +1000

    Based

--- python3/totoro_agent/brain/enemy_tracker.py
+++ python3/totoro_agent/brain/enemy_tracker.py
@@ -6,20 +6,21 @@ ammo, etc.
 
 class EnemyTracker:
     def __init__(self):
-        self.enemy_pos = [0, 0]
-
+        self.pos = [0, 0] # Don't need to be called 'enemy pos' -> it's in the enemytracker lol
+		self.health = 3 #fix these you bitch.  etc.
     def update(self, game_state):
         # TODO Yuv
         # own position
         # own health
         # own ammo
         # own diameter
-        # own invulnerable
+        # own invulnerable (we don't worry about our own stuff -- this'll be easily reflected)
 
         # enemy ammo
         # enemy position
         # enemy health
         # enemy diameter
         # enemy is invulnerable
+		is_invulnerable = #got hit or some shit in the past 5 ticks.? / set the ticks to when he is able to be hit again (similar to the website agent stats)
 
         pass
