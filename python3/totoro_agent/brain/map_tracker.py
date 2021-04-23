cdf834483229033b9a658ffd5c1049d68285704b
Chris Rabe <chris.rabe14@gmail.com>
2021-04-24 09:27:14 +1000

    tracked ores wood metal etc

--- python3/totoro_agent/brain/map_tracker.py
+++ python3/totoro_agent/brain/map_tracker.py
@@ -1,21 +1,31 @@
 """
 Used to track all information about the ores (their HP, etc)
 """
+from ..utils.constants import ENTITIES
 
 
 class MapTracker:
-    def __init__(self):
-        self.ores = []
 
     def update(self, game_state):
-        # TODO me
-        # hazard zones (blast tiles)
+        blast_blocks = []
+        ore_blocks = []
+        wood_blocks = []
+        metal_blocks = []
 
-        # ores (3 bomb hits) - health
-        # wood blocks
+        for entity in game_state.get("entities"):
+            if entity.get("type") == ENTITIES.get("blast"):
+                blast_blocks.append(entity)
+            elif entity.get("type") == ENTITIES.get("ore"):
+                ore_blocks.append(entity)
+            elif entity.get("type") == ENTITIES.get("wood"):
+                wood_blocks.append(entity)
+            elif entity.get("type") == ENTITIES.get("metal"):
+                metal_blocks.append(entity)
 
-        # metal blocks (walls)
+        game_state["blast_blocks"] = blast_blocks
+        game_state["ore_blocks"] = ore_blocks
+        game_state["wood_blocks"] = wood_blocks
+        game_state["metal_blocks"] = metal_blocks
 
         # TODO me and tony
         # pinch points of map - floyd warshall (research)
-        pass
