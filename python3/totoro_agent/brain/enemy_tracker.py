5aa91bbf6ea110536226855e4b567f97dfe3878f
Anti Matter <spiralknightsowner@gmail.com>
2021-04-24 09:06:37 +1000

    bug fixing

--- python3/totoro_agent/brain/enemy_tracker.py
+++ python3/totoro_agent/brain/enemy_tracker.py
@@ -5,35 +5,33 @@ ammo, etc.
 
 
 class EnemyTracker:
-    def __init__(self):
-		"""Enemy Tracker
-		Using enemy_ammo etc. even though it's THIS GUYS AMMO. we're not doubling reverse card. Enemy is OUR enemy. Global english, not relative."""
-        self.enemy_pos = [0, 0] # Don't need to be called 'enemy pos' -> it's in the enemytracker lol
+	def __init__(self):
+		#Enemy Tracker. Using enemy_ammo etc. even though it's THIS GUYS AMMO. we're not doubling reverse card. Enemy is OUR enemy. Global english, not relative.
+		self.enemy_pos = [0, 0] 
 		self.enemy_ammo = 3
-		self.enemy_health = 3 #fix these you bitch.  etc.
+		self.enemy_health = 3
 		self.enemy_blast_radius = 2 # -> Not checking # of powerups, so just increase this by 1 whenever we pick up a powerup.
 		self.enemy_invulnerable_until = 0 # Get better var name, number is inclusive.
 
 
-
-    def update(self, game_state):
-        # TODO Yuv
-        self.player_id = game_state["connection"]["agent_number"]
+	def update(self, game_state):
+		# TODO Yuv
+		self.player_id = game_state["connection"]["agent_number"]
 		# own position
-        # own health
-        # own ammo
-        # own diameter
-        # own invulnerable (we don't worry about our own stuff -- this'll be easily reflected)
+		# own health
+		# own ammo
+		# own diameter
+		# own invulnerable (we don't worry about our own stuff -- this'll be easily reflected)
 
 
-		self.enemy_id = game_state[(self.player_id+1)%2] # Could've just done 'not self.player_id' lol
-        self.enemy_ammo = game_state["agent_state"][self.enemy_id]["inventory"]["bombs"]  
+		self.enemy_id = not self.player_id # Could've just done 'not self.player_id' lol
+		self.enemy_ammo = game_state["agent_state"][self.enemy_id]["inventory"]["bombs"]  
 		self.enemy_pos = game_state["agent_state"][self.enemy_id]["coordinates"]
 		self.enemy_health = game_state["agent_state"][self.enemy_id]["hp"]
 		self.enemy_diameter = game_state["agent_state"][self.enemy_id]["blast_diameter"] # girthy boi
-		self.enemy_invulnerable_until = game_state['agent_state'][self.enemy_id]'[invulnerability'] # INCLUSIVE -> Vulnerable @ tick + 1 
+		self.enemy_invulnerable_until = game_state['agent_state'][self.enemy_id]['invulnerability'] # INCLUSIVE -> Vulnerable @ tick + 1 
 	#	is_invulnerable = #got hit or some shit in the past 5 ticks.? / set the ticks to when he is able to be hit again (similar to the website agent stats)
-#
+	#
 		# Shove them into game_state - Feels like there's a better way to do this but oops. Could also get rid of the above and just shove them in there.
 		game_state.enemy_id = self.enemy_id 
 		game_state.enemy_ammo = self.enemy_ammo 
@@ -41,3 +39,5 @@ class EnemyTracker:
 		game_state.enemy_health = self.enemy_health
 		game_state.enemy_diameter = self.enemy_diameter
 		game_state.enemy_invulnerable_until = self.enemy_diameter
+
+		pass
