382f478c7a04cb37eb6967a8c3434381bbd7992e
GauravRocketBooster <gau0003@suzannecoryhs.vic.edu.au>
2021-04-24 13:22:01 +1000

    Updated Pickup Tracker

--- python3/totoro_agent/brain/pickup_tracker.py
+++ python3/totoro_agent/brain/pickup_tracker.py
@@ -1,7 +1,7 @@
 """
 Used to track all information about the ores (their HP, etc)
 """
-
+from ..utils.util_functions import get_world_dimension 
 
 class PickupTracker:
     def __init__(self):
@@ -17,53 +17,35 @@ class PickupTracker:
         # Making empty lists for ammo and powerups. Will modify tick by tick.
         ammo_list = []
         powerup_list = []
+        
 
         # For loops for adding stuff to ammo_list and powerup_list 
         for a in game_state['entities']:
         	if a['type']=="a":
         		x = a['x']
         		y = a['y']
-        		ammo_list.append((x,y))
-
-        for b in game_state['entities']:
-        	if b['type']=="bp":
-        		x = b['x']
-        		y = b['y']
-        		powerup_list.append((x,y))
+        		# ammo_list.append(list((x,y)))
+        		ammo_list.append([x,y])
+        	if a['type']=="bp":
+        		x = a['x']
+        		y = a['y']
+        		# powerup_list.append(list((x,y)))
+        		powerup_list.append([x,y])
 
         # Player A and B's blast diameter updated tick by tick.'
-        player_a_wizard_diameter = game_state['agent_state']['0']['blast_diameter']
-        player_b_knight_diameter = game_state['agent_state']['1']['blast_diameter']
-        
+
         # Gets the map width and height.
-        map_width = game_state['world']['width']
-        map_height = game_state['world']['height']
+        map_width, map_height = get_world_dimension(game_state['world'])
 
-        # Checks if diameter of either playerA or playerB is greater than diameter. 
-        # If it is, it sets a variable to True, and if not False, and prints it to console.
-        if player_a_wizard_diameter > map_width or player_a_wizard_diameter > map_height:
-        	playerA_greater = True
-        	print("Player A's diameter is greater than Map.")
-        else:
-        	playerA_greater = False
-        	# print("Player A's diameter is smaller than the map.")
+        own_id = int(game_state['connection']['agent_number'])
+        enemy_id = int(1 - int(own_id))
 
-        if player_b_knight_diameter > map_width or player_b_knight_diameter > map_height:
-        	playerB_greater = True
-        	print("Player B's diameter is greater than Map.")
-        else:
-        	playerB_greater = False
-        	# print("Player B's diameter is smaller than the map.")
+        # Changes pickup priority depending on 
+        pickup_priority = "ammo_powerup"
+        if own_id >= max(map_width, map_height):
+        	pickup_priority = "ammo"
 
-
-        # Print statements that print the above stuff to the console. 
-        # Uncomment the top two for seeing stuff about diameters and the else statements to see more.
-
-        # print(f"Wizard or PlayerA's diameter is: {player_a_wizard_diameter}\n")
-        # print(f"Knight or PlayerB's diameter is: {player_b_knight_diameter}\n")
-        
-
-
-        print(f"There is ammo at: {ammo_list}\n")
-        print(f"There is powerups at: {powerup_list}\n")
-
+        # Setting the game_state parameters. 
+        game_state['ammo_list'] = ammo_list
+        game_state['powerup_list'] = powerup_list
+        game_state['pickup_priority'] = pickup_priority
