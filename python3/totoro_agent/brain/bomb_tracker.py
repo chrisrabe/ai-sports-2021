a03277ea43a518f124596acb4a42081163917289
Chris Rabe <chris.rabe14@gmail.com>
2021-04-24 13:49:02 +1000

    updates to bomb tracker

--- python3/totoro_agent/brain/bomb_tracker.py
+++ python3/totoro_agent/brain/bomb_tracker.py
@@ -2,17 +2,17 @@
 Used for tracking lifecycle of bombs and explosion areas
 """
 
-from ..utils.util_functions import get_blast_zone
+from ..utils.util_functions import get_blast_zone, get_entity_coords
+from ..utils.constants import ENTITIES
 
 class BombTracker:
     def __init__(self):
         self.bombs = []
 
     def update(self, game_state):
-        # TODO Will
         # Get own and enemy id
-        own_id = str(game_state['connection']['agent_number'])
-        enemy_id = str(1 - int(own_id))
+        own_id = game_state['player_id']
+        enemy_id = game_state['enemy_id']
         own_active_bombs = []
         enemy_active_bombs = []
 
@@ -21,13 +21,12 @@ class BombTracker:
 
         # Get active bombs
         for entity in entities:
-            if entity['owner'] == own_id and entity['type'] == 'b':
-                own_active_bombs.append([entity['x'], entity['y']])
-            elif entity['owner'] == enemy_id and entity['type'] == 'b':
-                enemy_active_bombs.append([entity['x'], entity['y']])
-            else:
-                pass
-            
+            if entity['type'] == ENTITIES['bomb']:
+                if entity['owner'] == own_id:
+                    own_active_bombs.append(entity)
+                elif entity['owner'] == enemy_id:
+                    enemy_active_bombs.append(entity)
+
         # Save values into game state
         game_state['own_active_bombs'] = own_active_bombs
         game_state['enemy_active_bombs'] = enemy_active_bombs
