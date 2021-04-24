f2e1ba3fba630f3fed2bdd643239ab8cdd12dc66
Chris Rabe <chris.rabe14@gmail.com>
2021-04-25 06:56:05 +1000

    added algorithm for retreat

--- python3/totoro_agent/strategies/retreat_strategy.py
+++ python3/totoro_agent/strategies/retreat_strategy.py
@@ -8,6 +8,8 @@ from ..utils.util_functions import get_shortest_path, get_path_action_seq
 class RetreatStrategy(strategy.Strategy):
 
     def execute(self, game_state: object) -> List[str]:
-        # find safest tile
+        # get all blast zones
+        # get tiles around it
+        # get the nearest safest tile
         # run to it!!
         return [ACTIONS["none"]]
