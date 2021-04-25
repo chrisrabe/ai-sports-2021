8ebdee6dee2788058e48667792a4527b1ed55a18
GauravRocketBooster <gau0003@suzannecoryhs.vic.edu.au>
2021-04-25 15:13:08 +1000

    Added Not working Pickup Strat

--- python3/totoro_agent/strategies/pickup_strategy.py
+++ python3/totoro_agent/strategies/pickup_strategy.py
@@ -1,7 +1,7 @@
 from typing import List
-
+import random
 from . import strategy
-from ..utils.util_functions import get_shortest_path, manhattan_distance
+from ..utils.util_functions import get_shortest_path, manhattan_distance, get_path_action_seq, get_blast_zone, get_nearest_tile
 from ..utils.constants import ACTIONS
 
 
@@ -12,4 +12,31 @@ class PickupStrategy(strategy.Strategy):
         # go for closest pickup powerup
         # else
         # go for closest ammo
-        return [ACTIONS["none"]]
+        player_pos = tuple(game_state['player_pos'])
+        ammo_list = game_state['ammo_list']
+        powerup_list = game_state['powerup_list']
+        pickup_priority = game_state['pickup_priority']
+        world = game_state['world']
+        entities = game_state['entities']
+
+        if pickup_priority == "ammo_powerup":
+            if ammo_list:
+                global ammo_choice
+                ammo_choice = random.choice(ammo_list[:])
+            if powerup_list:
+                global powerup_choice
+                powerup_choice = random.choice(powerup_list[:])
+
+            final_choice = random.choice(ammo_choice, powerup_choice)
+            global path
+            path = get_shortest_path(player_pos, final_choice, world, entities)
+            print(path)
+        elif pickup_priority == "ammo":
+            if ammo_list:
+                
+                ammo_choice = random.choice(ammo_list[:])
+                
+                path = get_shortest_path(player_pos, ammo_choice, world, entities)
+            print(path)
+
+        return get_path_action_seq(player_pos, path)
