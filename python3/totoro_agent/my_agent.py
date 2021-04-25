2404adfd33e54e00edebbe3759443db59bb429ea
Chris Rabe <chris.rabe14@gmail.com>
2021-04-25 10:48:25 +1000

    added algorithm for retrieving bombs

--- python3/totoro_agent/my_agent.py
+++ python3/totoro_agent/my_agent.py
@@ -20,6 +20,8 @@ class Agent:
         if tick_number - self.prev_tick != 1:
             print(f'Skipped a Tick: Tick #{tick_number}, skipped {tick_number - self.prev_tick}')
 
+        game_state['tick'] = tick_number
+
         if not self.action_queue:
             # Gets brain to eval environment, then spit out the strategy chosen (as string)
             strategy_name = self.brain.get_next_strategy(game_state)
--- python3/totoro_agent/strategies/retreat_strategy.py
+++ python3/totoro_agent/strategies/retreat_strategy.py
@@ -1,15 +1,30 @@
 from typing import List
+from copy import deepcopy
 
 from . import strategy
 from ..utils.constants import ACTIONS
-from ..utils.util_functions import get_shortest_path, get_path_action_seq
+from ..utils.util_functions import get_value_map, get_value_map_object, get_blast_zone
 
 
 class RetreatStrategy(strategy.Strategy):
 
+    def __init__(self):
+        self.reward_map = {
+            'potential_blast': -4,
+        }
+
     def execute(self, game_state: object) -> List[str]:
-        # get all blast zones
-        # get tiles around it
-        # get the nearest safest tile
-        # run to it!!
+        danger_bombs = deepcopy(game_state['enemy_active_bombs'])
+        own_bombs = game_state['own_active_bombs']
+        for bomb in own_bombs:
+            ttl = (bomb['expires'] - game_state['tick'])
+            if ttl == 1:  # need to GTFO!
+                danger_bombs.append(bomb)
+        blast_zones = []
+        for bomb in danger_bombs:
+            pass
+        # combine blast zones
+        # convert to value map objects
+        # get value map
+        # get the adjacent tile that has the maximum value
         return [ACTIONS["none"]]
