c967f564202db93915d0022dcdba8e91e1902147
Chris Rabe <chris.rabe14@gmail.com>
2021-05-01 04:58:53 +1000

    updated retreat strat

--- python3/agents/shared/strategies/retreat_strategy.py
+++ python3/agents/shared/strategies/retreat_strategy.py
@@ -3,7 +3,7 @@ from typing import List
 from . import strategy
 from ..utils.constants import ACTIONS
 from ..utils.util_functions import get_value_map, get_value_map_objects_from_arr, get_move_from_value_map, \
-    get_reachable_tiles, move_results_in_ouchie
+    get_reachable_tiles, move_results_in_ouchie, convert_entities_to_coords
 
 
 class RetreatStrategy(strategy.Strategy):
@@ -20,6 +20,7 @@ class RetreatStrategy(strategy.Strategy):
         world = game_state['world']
         safe_zones = game_state['safe_zones']
         enemy_pos = game_state['enemy_pos']
+        blast_zones = convert_entities_to_coords(game_state['blast_blocks'])
         if enemy_pos in safe_zones:
             safe_zones.remove(enemy_pos)
         player_pos = game_state['player_pos']
@@ -32,7 +33,7 @@ class RetreatStrategy(strategy.Strategy):
         game_objects = hazard_objects + safe_objects + reachable_objects
         value_map = get_value_map(world, game_state["wall_blocks"], game_objects, self.reward_map, use_default=False)
         action = get_move_from_value_map(game_state["player_pos"], value_map, world)
-        if move_results_in_ouchie(player_pos, action, game_state['hazard_zones']):
+        if move_results_in_ouchie(player_pos, action, blast_zones):
             return [ACTIONS['none']]
         else:
             return [action]
