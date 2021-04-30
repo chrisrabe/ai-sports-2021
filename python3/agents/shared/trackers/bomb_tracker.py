1b9538744b315fb4a69fed284fc503c795d9f029
Anti Matter <spiralknightsowner@gmail.com>
2021-04-30 20:51:02 +1000

    Pogging out.
    Algo bot is fucking pog.

--- python3/agents/shared/trackers/bomb_tracker.py
+++ python3/agents/shared/trackers/bomb_tracker.py
@@ -3,9 +3,8 @@ Used for tracking lifecycle of bombs and explosion areas
 """
 import math
 
-from ..utils.util_functions import get_blast_zone, get_safe_tiles, get_surrounding_tiles, get_world_dimension
+from ..utils.util_functions import get_blast_zone, get_safe_tiles, get_surrounding_tiles, get_world_dimension, get_surrounding_empty_tiles, dummy_bomb, get_shortest_path
 from ..utils.constants import ENTITIES
-
 
 class BombTracker:
     def __init__(self):
@@ -128,4 +127,42 @@ class BombTracker:
                 'type': ENTITIES['enemy']
             })
 
-      #  game_state['enemy_immediate_trapped'] = death_trap(game_state['enemy_pos'], world, entities)
+      ###### Trapping Detection #####
+        game_state['enemy_immediate_trapped'] = False
+        game_state['enemy_onestep_trapped'] = False
+
+        # # Return any tiles that are empty around enemy
+        enemy_surrounding_tiles = get_surrounding_tiles(game_state['enemy_pos'], world_width, world_height)
+        enemy_surrounding_empty_tiles = get_surrounding_empty_tiles(game_state['enemy_pos'], world, entities, ignore_player = False) # Needs to Include us 
+        if len(enemy_surrounding_empty_tiles) == 0:  # Dude can't move. Technically, this 'immediate trapped' isn't the real value. It's actually trapped AND player is one of the tiles.
+            # check if our player is in one of the tiles: ->
+            if game_state['player_pos'] in enemy_surrounding_tiles:  # Make sure
+                game_state['enemy_immediate_trapped'] = True
+
+        ###### Extended trapping detection ######
+        # Dummy bomb to see if the enemy would be trapped (for 1 move: Initial + 1 move)
+        virtual_bomb = dummy_bomb(game_state['player_pos'], game_state['player_diameter'])
+        blast_zone = get_blast_zone(virtual_bomb['coord'], virtual_bomb['blast_diameter'], entities, world)
+        if game_state['enemy_pos'] in blast_zone:
+            check =  all(item in blast_zone for item in enemy_surrounding_empty_tiles) # checks if list one contains all elements of list 2
+            if check:
+                game_state['enemy_onestep_trapped'] = True
+
+
+        # Removes player from enemy surround tiles
+        if game_state['player_pos'] in enemy_surrounding_empty_tiles:
+            self.stare_contest_duration += 1
+            if self.stare_contest_duration == MAX_STARE_CONTEST_DURATION:
+                game_state['tell_enemy_gtfo'] = True
+            enemy_surrounding_empty_tiles.remove(
+                game_state['player_pos'])  # Remove player pos from surround (why is he in there lol)
+        else:
+            self.stare_contest_duration = 0  # reset
+
+        print("Player pos:", game_state['enemy_pos'], "Length of enem surrounding empty tiles: ",
+              len(enemy_surrounding_empty_tiles), enemy_surrounding_empty_tiles,
+              print(len(enemy_surrounding_empty_tiles) == 0))
+
+        # check if there's a clear path to the enemy
+        path = get_shortest_path(game_state['player_pos'], game_state['enemy_pos'], world, entities)
+        game_state['clear_path_to_enemy'] = path is not None
--- python3/agents/totoro_agent/brain.py
+++ python3/agents/totoro_agent/brain.py
@@ -68,7 +68,7 @@ class Brain:
         elif len(game_state['pickup_list']) != 0:  # "Any pickups on the map?"
             print('me gusta I smell some pickups')
             return 'pickup'
-        elif not game_state['clear_path_to_enemy']:
+        elif not game_state['clear_path_to_enemy'] and game_state['player_pos'] in game_state['hazard_zones']:
             return 'block_destroy'
         else:
             print("I'ma stalk.")
