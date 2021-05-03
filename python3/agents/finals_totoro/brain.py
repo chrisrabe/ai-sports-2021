617f4d52132ebb00b46e715bf4856dfe14e06ca9
Anti Matter <spiralknightsowner@gmail.com>
2021-05-03 16:40:18 +1000

    Update brain.py

--- python3/agents/finals_totoro/brain.py
+++ python3/agents/finals_totoro/brain.py
@@ -17,13 +17,13 @@ class Brain:
 
         print("I am totoro finals bot")
 
-        # if game_state['player_pos'] in game_state['all_hazard_zones']:
-        #     return 'retreat'
+        if game_state['player_pos'] in game_state['all_hazard_zones']:
+            return 'retreat'
 
-        # self.benchmark.start('detonate')
-        # if not game_state['enemy_is_invulnerable'] and game_state['enemy_pos'] in game_state['detonation_zones']:
-        #     return 'detonate'
-        # self.benchmark.end('detonate')
+        self.benchmark.start('detonate')
+        if not game_state['enemy_is_invulnerable'] and game_state['enemy_pos'] in game_state['detonation_zones']:
+            return 'detonate'
+        self.benchmark.end('detonate')
 
         # # Killing strategies
         if game_state['player_inv_bombs'] > 0:
@@ -32,22 +32,20 @@ class Brain:
             self.benchmark.end('trap')
             if game_state['enemy_immediate_trapped']:
                 return 'simple_bomb'
-                
-        return 'wait'
 
-        #     self.benchmark.start('onestep')
-        #     self.finals_tracker.update_onestep(game_state)
-        #     self.benchmark.end('onestep')
-        #     if game_state['enemy_onestep_trapped']:
-        #         return 'simple_bomb'
+            self.benchmark.start('onestep')
+            self.finals_tracker.update_onestep(game_state)
+            self.benchmark.end('onestep')
+            if game_state['enemy_onestep_trapped']:
+                return 'simple_bomb'
 
-        # self.benchmark.start('path')
-        # self.finals_tracker.update_path(game_state)
-        # self.benchmark.end('path')
+        self.benchmark.start('path')
+        self.finals_tracker.update_path(game_state)
+        self.benchmark.end('path')
 
-        # if len(game_state['pickup_list']) != 0:
-        #     return 'pickup'
-        # elif not game_state['clear_path_to_enemy'] and game_state['player_inv_bombs'] > 2:
-        #     return 'block_destroy'
-        # else:
-        #     return 'stalk'
+        if len(game_state['pickup_list']) != 0:
+            return 'pickup'
+        elif not game_state['clear_path_to_enemy'] and game_state['player_inv_bombs'] > 2:
+            return 'block_destroy'
+        else:
+            return 'stalk'
