72e45efc9b236fcd4538465f3995cacd7bd7ad55
GauravRocketBooster <gau0003@suzannecoryhs.vic.edu.au>
2021-05-04 21:44:02 +1000

    added player_had_control

--- python3/agents/shared/utils/util_functions.py
+++ python3/agents/shared/utils/util_functions.py
@@ -678,7 +678,25 @@ def get_move_from_value_map(cur_loc, value_map, world):
             max_val = tile_val
             new_loc = tile
     return move_to_tile(cur_loc, new_loc)
+def player_has_control(player_pos, enemy_pos, world) -> bool:
+        world_width, world_height = get_world_dimension(world)
+        world_corners = [(0,0), (world_width-1, 0), (0, world_height-1), (world_width-1, world_height-1)]
 
+        # check if enemy is closer to world edge than player
+        min_player_corner_dist = min(manhattan_distance(player_pos, world_corners[0]), 
+                                     manhattan_distance(player_pos, world_corners[1]), 
+                                     manhattan_distance(player_pos, world_corners[2]), 
+                                     manhattan_distance(player_pos, world_corners[3]))
+
+        min_enemy_corner_dist = min(manhattan_distance(enemy_pos, world_corners[0]), 
+                                    manhattan_distance(enemy_pos, world_corners[1]), 
+                                    manhattan_distance(enemy_pos, world_corners[2]), 
+                                    manhattan_distance(enemy_pos, world_corners[3]))
+
+        player_enemy_dist = manhattan_distance(player_pos, enemy_pos)
+
+        # get number of empty tiles in the enemy surrounding that meets the condition for attack engagement
+        return min_player_corner_dist > min_enemy_corner_dist
 
 def get_articulation_points(player_loc, world, entities) -> list[tuple[int, int]]:
     """
